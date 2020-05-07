b3599edce1f4252aafb2a67a5778f7eee4820201
Jaimedgp <jaime.diez.gp@gmail.com>
2020-05-07 20:30:15 +0200

    Start Multiplicative encryption module

---
+++ encrypt/Multiplicative.py
@@ -0,0 +1,10 @@
+def encrypt(pass_key, key):
+    """ Encrypt pass_key using multiplicative algorithm """
+
+    pass
+
+
+def decipher(pass_key, key):
+    """ Decipher pass_key using multiplicative algorithm """
+
+    pass
