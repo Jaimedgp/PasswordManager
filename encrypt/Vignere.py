e6855df851d0f1192848c14a58999db5d6cafc21
Jaimedgp <jaime.diez.gp@gmail.com>
2020-05-07 20:33:12 +0200

    Start Vignere encryption module

---
+++ encrypt/Vignere.py
@@ -0,0 +1,10 @@
+def encrypt(pass_key, subkey):
+    """ Encrypt pass_key using Vignere algorithm """
+
+    pass
+
+
+def decipher(pass_key, subkey):
+    """ Decipher pass_key using Vignere algorithm """
+
+    pass
