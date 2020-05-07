f3602f3be68a82001c0e64c0ff84fc94a4d070e4
Jaimedgp <jaime.diez.gp@gmail.com>
2020-05-07 20:27:14 +0200

    Start Ceasar encryption module

---
+++ encrypt/Ceasar.py
@@ -0,0 +1,10 @@
+def encrypt(pass_key, shift):
+    """ Encrypt pass_key using ceasar algorithm """
+
+    pass
+
+
+def decipher(pass_key, shift):
+    """ Decipher pass_key using ceasar algorithm """
+
+    pass
