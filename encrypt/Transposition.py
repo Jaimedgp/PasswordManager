dcd7e2f9a6224ccb2b109fb777642e677cbcbdb1
Jaimedgp <jaime.diez.gp@gmail.com>
2020-05-07 20:27:44 +0200

    Start Transposition encryption module

---
+++ encrypt/Transposition.py
@@ -0,0 +1,10 @@
+def encrypt(pass_key, width):
+    """ Encrypt pass_key using transposition algorithm """
+
+    pass
+
+
+def decipher(pass_key, width):
+    """ Decipher pass_key using transposition algorithm """
+
+    pass
