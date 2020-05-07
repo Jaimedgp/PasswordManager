900f83a2ecfc537276284916df2f263d5c17e1b5
Jaimedgp <jaime.diez.gp@gmail.com>
2020-05-07 22:16:36 +0200

    Drawback encryption function

--- encrypt/Drawback.py
+++ encrypt/Drawback.py
@@ -1,10 +1,10 @@
 def encrypt(pass_key):
     """ Encrypt pass_key using drawback algorithm """
 
-    pass
+    return pass_key[::-1]
 
 
 def decipher(pass_key):
     """ Decipher pass_key using drawback algorithm """
 
-    pass
+    return pass_key[::-1]
