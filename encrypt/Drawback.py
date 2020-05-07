60443527a21d2b98fe776ce7ee000a8e33b9e41d
Jaimedgp <jaime.diez.gp@gmail.com>
2020-05-07 20:13:49 +0200

    Start Drawback encryption module

---
+++ encrypt/Drawback.py
@@ -0,0 +1,10 @@
+def encrypt(pass_key):
+    """ Encrypt pass_key using drawback algorithm """
+
+    pass
+
+
+def decipher(pass_key):
+    """ Decipher pass_key using drawback algorithm """
+
+    pass
