6f3e3ca06ed641b81aab6175643ad9c5eaabc28e
Jaimedgp <jaime.diez.gp@gmail.com>
2020-05-07 22:41:33 +0200

    Ceasar encrypt function (decipher still not done)

--- encrypt/Ceasar.py
+++ encrypt/Ceasar.py
@@ -1,7 +1,21 @@
+dictionary = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        + "abcdefghijklmnopqrstuvwxyz"
+        + "1234567890"
+        +",.?!@#$%&*(){}[]:;-_=+ ")
+
 def encrypt(pass_key, shift):
     """ Encrypt pass_key using ceasar algorithm """
 
-    pass
+    cipher = ""
+
+    for letter in pass_key:
+        i = dictionary.index(letter) + shift
+
+        i -= len(dictionary) if i >= len(dictionary) else 0
+
+        cipher += dictionary[i]
+
+    return cipher
 
 
 def decipher(pass_key, shift):
