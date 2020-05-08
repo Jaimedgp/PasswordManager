7d42faeed7776906eb611c566a94474761a05c65
Jaimedgp <jaime.diez.gp@gmail.com>
2020-05-08 14:19:34 +0200

    Transposition encrypt function (decipher still not done)

--- encrypt/Transposition.py
+++ encrypt/Transposition.py
@@ -1,7 +1,19 @@
+import numpy as np
+
 def encrypt(pass_key, width):
     """ Encrypt pass_key using transposition algorithm """
 
-    pass
+    numRows = int(len(pass_key) / width + 0.5)
+
+    characters = [letter for letter in pass_key]
+    while len(characters) < width*numRows:
+        characters.append("<")
+
+    board = np.array(characters).reshape((numRows, width))
+    print(board)
+    cipher = "".join(["".join(board[:, i]) for i in range(width)])
+
+    return cipher
 
 
 def decipher(pass_key, width):
