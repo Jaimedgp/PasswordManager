2c456610b5cfe7e3dd1aec4293c4cedb79c034a1
Jaimedgp <jaime.diez.gp@gmail.com>
2020-05-08 16:28:29 +0200

    Add Vignere encrypt and decipher module

--- encrypt/Vignere.py
+++ encrypt/Vignere.py
@@ -1,10 +1,34 @@
+alphabet = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+            + "abcdefghijklmnopqrstuvwxyz"
+            + "1234567890"
+            +",.?!@#$%&*(){}[]:;-_=+ ")
+
+
 def encrypt(pass_key, subkey):
     """ Encrypt pass_key using Vignere algorithm """
 
-    pass
+    cipher = ""
+
+    for pk_letter, sk_letter in zip(pass_key, subkey):
+        pk_i = alphabet.index(pk_letter)
+        sk_i = alphabet.index(sk_letter)
+
+        new_i = (pk_i + sk_i) % len(alphabet)
+        cipher += alphabet[new_i]
+
+    return cipher
 
 
 def decipher(pass_key, subkey):
     """ Decipher pass_key using Vignere algorithm """
 
-    pass
+    cipher = ""
+
+    for pk_letter, sk_letter in zip(pass_key, subkey):
+        pk_i = alphabet.index(pk_letter)
+        sk_i = alphabet.index(sk_letter)
+
+        new_i = (pk_i - sk_i) % len(alphabet)
+        cipher += alphabet[new_i]
+
+    return cipher
