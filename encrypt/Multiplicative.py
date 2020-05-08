e205081b84316fff1e3fc1fe807802f7470889bc
Jaimedgp <jaime.diez.gp@gmail.com>
2020-05-08 15:58:21 +0200

    Encrypt function using Multiplicative

--- encrypt/Multiplicative.py
+++ encrypt/Multiplicative.py
@@ -1,7 +1,21 @@
+alphabet = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+            + "abcdefghijklmnopqrstuvwxyz"
+            + "1234567890"
+            +",.?!@#$%&*(){}[]:;-_=+ ")
+
+
 def encrypt(pass_key, key):
     """ Encrypt pass_key using multiplicative algorithm """
 
-    pass
+    cipher = ""
+
+    for letter in pass_key:
+        i = alphabet.index(letter)
+        new_i = (i*key) % len(alphabet)
+
+        cipher += alphabet[new_i]
+
+    return cipher
 
 
 def decipher(pass_key, key):
