1bd20d2d6cb97803308685c7d0b40fe0ff897f0a
Jaimedgp <jaime.diez.gp@gmail.com>
2020-05-09 19:37:07 +0200

    Add more functions to database

--- database.py
+++ database.py
@@ -15,7 +15,7 @@ class DataBase():
 
         # Check if database exists yet
         result = self.create_database()
-        if result == 0:
+        if result:
             print("Dabase exists")
 
 
@@ -24,14 +24,14 @@ class DataBase():
 
         try:
             self.connect.execute("""
-CREATE TABLE PASS_KEY (
-    idpass INTEGER PRIMARY KEY AUTOINCREMENT,
-    pass_key VARCHAR(200) NOT NULL UNIQUE,
-    service VARCHAR(200) NOT NULL,
-    account VARCHAR(200) NOT NULL,
+                CREATE TABLE PASS_KEY (
+                    idpass INTEGER PRIMARY KEY AUTOINCREMENT,
+                    pass_key VARCHAR(200) NOT NULL UNIQUE,
+                    service VARCHAR(200) NOT NULL,
+                    account VARCHAR(200) NOT NULL,
 
-    CONSTRAINT unq_srvc_accnt UNIQUE (service, account)
-    ); """)
+                    CONSTRAINT unq_srvc_accnt UNIQUE (service, account)
+                ); """)
         except:
             return 0
 
@@ -41,10 +41,10 @@ CREATE TABLE PASS_KEY (
 
         try:
             pass_key = self.connect.execute("""
-SELECT pass_key
-    FROM PASS_KEY pssKy
-    WHERE pssKy.service = '%s' AND pssKy.account = '%s';
-""" %(service, account))
+                SELECT pass_key
+                    FROM PASS_KEY pssKy
+                    WHERE pssKy.service = '{0}' AND pssKy.account = '{1}';
+                """.format(service, account))
 
             return pass_key
         except:
@@ -56,8 +56,63 @@ SELECT pass_key
 
         try:
             self.connect.execute("""
-INSERT INTO PASS_KEY(pass_key, service, account)
-    VALUES ('{0}', '{1}', '{2}');
-""".format(pass_key, service, account))
+                INSERT INTO PASS_KEY(pass_key, service, account)
+                    VALUES ('{0}', '{1}', '{2}');
+                """.format(pass_key, service, account))
+
+            return 1
         except:
             return 0
+
+
+    def change_pass_key(self, new_key, service, account):
+        """ Change actual password for a new one """
+
+        try:
+            self.connect.execute("""
+                UPDATE PASS_KEY
+                    SET pass_key='{0}'
+                    WHERE service='{1}' AND account='{2}';
+                """.format(new_key, service, account))
+
+            self.connect.commit()
+            return 1
+        except:
+            return 0
+
+
+    def get_accounts(self, service):
+        """ Get all the accounts used in a service """
+
+        try:
+            accounts = self.connect.execute("""
+                SELECT pssKy.account
+                    FROM PASS_KEY pssKy
+                    WHERE pssKy.service = '{0}';
+                """.format(service))
+
+            return accounts
+        except:
+            return 0
+
+
+    def get_services(self, account):
+        """ Get all services register with an account """
+
+        try:
+            services = self.connect.execute("""
+                SELECT pssKy.service
+                    FROM PASS_KEY pssKy
+                    WHERE pssKy.account = '{0}';
+                """.format(account))
+
+            return services
+        except:
+            return 0
+
+
+    def drop_tables(self):
+        """ delete all the tables of the database """
+
+        self.connect.execute("DROP TABLE PASS_KEY;")
+        self.connect.commit()
