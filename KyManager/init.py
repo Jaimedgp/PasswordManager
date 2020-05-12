41be75f2b4f99331ea49f8ae0ea6aa494cb1958a
Jaimedgp <jaime.diez.gp@gmail.com>
2020-05-12 16:57:22 +0200

    Add docstring variables

--- KyManager/init.py
+++ KyManager/init.py
@@ -5,3 +5,10 @@
     @author Jaimedgp
     @date May,2020
 """
+
+__author__ = "Jaime Diez Gonzalez-Pardo"
+__version__ = "0.1"
+__date__ = "May, 2020"
+__maintainer__ = "Jaime Diez Gonzalez-Pardo"
+__email__ = "jaime.diez.gp@gmail.com"
+__status__ = "Development"
